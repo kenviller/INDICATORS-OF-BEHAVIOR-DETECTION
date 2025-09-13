296a153219fef5812833e835cbb667155a63576c
kenviller <geokenvil@gmail.com>
2025-09-13 16:05:00 +0530

    Delete iot-iob-detector/iot-iob-detector directory

--- iot-iob-detector/iot-iob-detector/agent/config.py
+++
@@ -1,3 +0,0 @@
-
-trusted_ports = [80, 443, 1883]
-suspicious_threshold = 5
--- iot-iob-detector/iot-iob-detector/tests/simulate_mqtt_flood.py
+++
@@ -1,5 +0,0 @@
-
-import os
-broker = "192.168.1.10"
-for i in range(10):
-    os.system(f"mosquitto_pub -h {broker} -t 'iot/device/cmd' -m 'hack_{i}'")
